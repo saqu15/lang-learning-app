7e49dd671a5b8adcf9c0b4aba5414576a8d0ccd6
Szymon <szymon071200@gmail.com>
2023-11-13 16:46:48 +0100

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ButtonModule } from 'primeng/button';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TestComponentComponent } from './test-component/test-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
