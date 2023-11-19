21be18037d1260836355c2b671d6ccad71270106
Szymon <szymon071200@gmail.com>
2023-11-19 17:54:05 +0100

    navbar added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem, MessageService } from 'primeng/api';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.sass'],
+	providers: [MessageService],
 })
-export class AppComponent {
-	title = 'praca-inzynierska';
+export class AppComponent implements OnInit {
+	items: MenuItem[] | undefined;
+
+	ngOnInit() {
+		this.items = [
+			{
+				label: 'Options',
+				items: [
+					{
+						label: 'Update',
+						icon: 'pi pi-refresh',
+					},
+					{
+						label: 'Delete',
+						icon: 'pi pi-times',
+					},
+				],
+			},
+			{
+				label: 'Navigate',
+				items: [
+					{
+						label: 'Angular',
+						icon: 'pi pi-external-link',
+						url: 'http://angular.io',
+					},
+					{
+						label: 'Router',
+						icon: 'pi pi-upload',
+						routerLink: '/test',
+					},
+				],
+			},
+		];
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonModule } from 'primeng/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponentComponent } from './test-component/test-component.component';
+import { MenuModule } from 'primeng/menu';
+import { ToastModule } from 'primeng/toast';
+import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
 	declarations: [AppComponent, TestComponentComponent],
-	imports: [BrowserModule, AppRoutingModule, ButtonModule],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		AppRoutingModule,
+		ButtonModule,
+		MenuModule,
+		ToastModule,
+		MenubarModule,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
