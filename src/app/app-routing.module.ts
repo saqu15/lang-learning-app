3047121229a0e10c7221ebbfe1b745b4796f610d
Szymon <szymon071200@gmail.com>
2023-11-24 21:04:39 +0100

    added auth guard and controls for login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TestComponentComponent } from './test-component/test-component.component';
 import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
+import { AuthGuard } from './util/guards/auth.guard';
 
 const routes: Routes = [
-	{ path: 'test', component: TestComponentComponent },
+	{
+		path: 'test',
+		component: TestComponentComponent,
+		canActivate: [AuthGuard],
+	},
 	{ path: 'login', component: LoginComponent },
+	{ path: '', component: AppComponent, canActivate: [AuthGuard] },
+	{ path: '*', component: AppComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
