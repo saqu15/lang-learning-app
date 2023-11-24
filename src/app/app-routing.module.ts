30585610ba8e1205663ab623983c64d9a1d8af18
Szymon <szymon071200@gmail.com>
2023-11-24 20:13:25 +0100

    login component base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TestComponentComponent } from './test-component/test-component.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [{ path: 'test', component: TestComponentComponent }];
+const routes: Routes = [
+	{ path: 'test', component: TestComponentComponent },
+	{ path: 'login', component: LoginComponent },
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
